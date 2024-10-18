4c4f61f74d201a1111025b8243f72b26efddab83
brandon-irving <birvingfau@gmail.com>
2024-10-17 20:48:50 -0400

    hard coded dev branch

--- src/lib/provider.tsx
+++ src/lib/provider.tsx
@@ -3,7 +3,10 @@
 import { ConvexProvider, ConvexReactClient } from 'convex/react';
 import { ReactNode } from 'react';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || '');
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL ||
+    'https://cautious-parrot-972.convex.cloud',
+);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
